c80a080dc56401bba5098ba34491cb89616d1097
LukasV <lukas@code8.cz>
2020-06-30 16:46:25 +0200

    Fragment cannot have a className attribute

--- src/pages/components/HeadWithInputs.js
+++ src/pages/components/HeadWithInputs.js
@@ -5,62 +5,56 @@ import Grid from '@material-ui/core/Grid'
 import Head from './Head'
 import Inputs from './Inputs'
 
-const useStyles = makeStyles(theme => ({
-    root: {
+const useStyles = makeStyles((theme) => ({
+  root: {},
+  clear: {
+    clear: 'both',
+  },
+  head: {
+    textAlign: 'left',
+    '&:after': {
+      content: `''`,
+      display: 'table',
+      clear: 'both',
     },
-    clear: {
-        clear: 'both',
-    },
-    head: {
-        textAlign: 'left',
-        '&:after': {
-            content: `''`,
-            display: 'table',
-            clear: 'both'
-        },
 
-        [theme.breakpoints.up('sm')]: {
-        },
-        [theme.breakpoints.up('md')]: {
-            float: 'left',
-            width: 520,
-        },
+    [theme.breakpoints.up('sm')]: {},
+    [theme.breakpoints.up('md')]: {
+      float: 'left',
+      width: 520,
     },
-    inputs: {
-        textAlign: 'left',
-        padding: theme.spacing(0, 0, 2, 0),
-        minWidth: 300,
-        [theme.breakpoints.up('sm')]: {
-            padding: theme.spacing(0, 0, 2, 0),
-        },
-        [theme.breakpoints.up('sm')]: {
-        },
-        [theme.breakpoints.up('md')]: {
-            float: 'right',
-            padding: theme.spacing(0),
-            width: 'calc(100% - 320px)',
-        },
-    }
+  },
+  inputs: {
+    textAlign: 'left',
+    padding: theme.spacing(0, 0, 2, 0),
+    minWidth: 300,
+    [theme.breakpoints.up('sm')]: {
+      padding: theme.spacing(0, 0, 2, 0),
+    },
+    [theme.breakpoints.up('sm')]: {},
+    [theme.breakpoints.up('md')]: {
+      float: 'right',
+      padding: theme.spacing(0),
+      width: 'calc(100% - 320px)',
+    },
+  },
 }))
 
 const HeadWithInputs = ({ title, disableInputs, disableClear, classAdd }) => {
-    const classes = useStyles()
+  const classes = useStyles()
 
-    return (
-        <Fragment className={classes.headContainer}>
-            <div style={{marginBottom:80}}>
-            <Grid item md={6} xs={12} className={classes.head}>
-                <Head
-                    title={title}
-                    classAdd={classAdd}
-                />
-            </Grid>
-            <Grid item md={6} xs={12} className={classes.inputs}>
-                {disableInputs !== true && <Inputs />}
-            </Grid>
-            {disableClear !== true && <div className={classes.clear}></div>}
-            </div>
-        </Fragment>
-    )
+  return (
+    <div className={classes.headContainer}>
+      <div style={{ marginBottom: 80 }}>
+        <Grid item md={6} xs={12} className={classes.head}>
+          <Head title={title} classAdd={classAdd} />
+        </Grid>
+        <Grid item md={6} xs={12} className={classes.inputs}>
+          {disableInputs !== true && <Inputs />}
+        </Grid>
+        {disableClear !== true && <div className={classes.clear}></div>}
+      </div>
+    </div>
+  )
 }
 export default HeadWithInputs
