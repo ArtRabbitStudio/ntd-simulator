35a57a007a12ac581552dda4fc981027b2e6f5cd
Tom Elsner <tom@artrabbit.com>
2020-07-01 18:25:31 +0100

    Refined ScenarioGraph output showing historical / future data

--- src/components/ScenarioGraph.js
+++ src/components/ScenarioGraph.js
@@ -23,31 +23,44 @@ function ScenarioGraph({
 }) {
   //console.log('init',data);
   const startYear = 15
+  const futureYear = 20
+  const lPad = 50
+  const rPad = 32
+  const tPad = 20
+  const yPad = 32+20
+  const svgHeight = height + yPad * 2
+  const svgWidth = width
+  
   const dataSelection = showAllResults ? data.results : [data.results[0]]
 
   const isStartYear = (element) => element >= startYear;
+  const isPrediction = (element) => element >= futureYear;
 
   const IndexToStartForOutput = (flatten(map(dataSelection, 'ts')).findIndex(isStartYear))
+  const IndexForPrediction = (flatten(map(dataSelection, 'ts')).findIndex(isPrediction))
   const domainX = [ startYear, max(flatten(map(dataSelection, 'ts')))+1 ]
 
   const domainY = extent(
     flattenDeep(map( data.results , (x) => values(pick(x, metrics))))
   )
-  const ShowActivePoint = ({ active, coord }) => {
+  const ShowActivePoint = ({ active, coord, mode }) => {
+    const highColour = mode === 'f' ? '#D86422' : '#A3A3A3'
+    const lowColour =  mode === 'f' ? '#03D386' : '#A8CFC0'
+
     return (
       <g
-        key={`active-${active}`}
+        key={`active-${active}-${mode}`}
         transform={`translate(${coord[0]},${coord[1]})`}
       >
         <circle
           fill={
             coord[2] <= 1
-              ? '#008DC9'
+              ? lowColour
               : coord[2] >= 6 && coord[2] <= 10
-              ? '#D86422'
+              ? highColour
               : coord[2] > 10
-              ? '#D86422'
-              : '#D86422'
+              ? highColour
+              : highColour
           }
           fillOpacity={1}
           r={20}
@@ -69,17 +82,16 @@ function ScenarioGraph({
       </g>
     )
   }
-  const InfoPoints = ({ data, prop, x, y, color }) => {
+  const InfoPoints = ({ data, prop, x, y, color, mode }) => {
     const coords = data.map((d) => [x(d.ts), y(d[prop]), d[prop]])
-    //console.log(coords);
     let points = null
 
     points = coords.map((coord, i) => {
       return (
-        <g key={`info-${i}`} transform={`translate(${coord[0]},${coord[1]})`}>
+        <g key={`info-${i}-${mode}`} transform={`translate(${coord[0]},${coord[1]})`}>
           <circle
-            key={`${i}`}
-            fill={'#D86422'}
+            key={`${i}-${mode}`}
+            fill={color}
             fillOpacity={0.8}
             r={3}
             cx={0}
@@ -88,23 +100,19 @@ function ScenarioGraph({
         </g>
       )
     })
-    if (activeInfo != null) {
-      points.push(
-        <ShowActivePoint active={activeInfo} coord={coords[activeInfo]} />
-      )
-    }
+    
     let hoverPoints = coords.map((coord, i) => {
       return (
-        <g key={`hover-${i}`} transform={`translate(${coord[0]},${coord[1]})`}>
+        <g key={`hover-${i}-${mode}`} transform={`translate(${coord[0]},${coord[1]})`}>
           <circle
-            key={`${i}-h`}
-            fill={'#D86422'}
+            key={`${i}-${mode}-h`}
+            fill={color}
             fillOpacity={0}
             r={6}
             cx={0}
             cy={0}
             cursor="crosshair"
-            onMouseEnter={() => handleEnter(i)}
+            onMouseEnter={() => handleEnter(`${i}-${mode}`)}
             onMouseLeave={handleLeave}
           ></circle>
         </g>
@@ -129,12 +137,7 @@ function ScenarioGraph({
     }, 50)
   }
 
-  const lPad = 50
-  const rPad = 32
-  const tPad = 20
-  const yPad = 32
-  const svgHeight = height + yPad * 2
-  const svgWidth = width
+  
 
 
   const x = scaleLinear().domain(domainX).range([0, width-rPad])
@@ -149,33 +152,79 @@ function ScenarioGraph({
     const { ts, Ms, Ws, Ls } = d
     const series = zip(ts, Ms, Ws, Ls)
     let seriesObj = map(series, (x) => zipObject(['ts', 'Ms', 'Ws', 'Ls'], x))
-    seriesObj.splice(0,IndexToStartForOutput)
+    //seriesObj.splice(0,IndexToStartForOutput)
+    // historic path
+    const historicSeries = seriesObj.slice(IndexToStartForOutput,IndexForPrediction+1)
+    const futureSeries = seriesObj.slice(IndexForPrediction)
+    // future path
 
     const color = main ? '#D86422' : '#eeee'
+    const hcolor = main ? '#CCCCCC' : '#eeee'
+
+    let activeCoords = []
+    let activeMode = 'h'
+    if (activeInfo != null) {
+      const activeInfoParts = activeInfo.split('-');
+      activeCoords = historicSeries[activeInfoParts[0]]//.map((d) => [x(d.ts), y(d[prop]), d[prop]]);
+      if (activeInfoParts[1] === 'f') {
+        activeMode = 'f'
+        activeCoords = futureSeries[activeInfoParts[0]]//.map((d) => [x(d.ts), y(d[prop]), d[prop]]);
+      } 
+   
+    }
+
+ 
+
     return (
       <>
         {metrics.map((m,i) => (
+          <g key={`${i}-l`}>
           <Path
-            key={`${i}-l`}
-            data={seriesObj}
+            key={`${i}-l-h`}
+            data={historicSeries}
+            prop={m}
+            x={x}
+            y={y}
+            color={hcolor}
+          />
+          <Path
+            key={`${i}-l-f`}
+            data={futureSeries}
             prop={m}
             x={x}
             y={y}
             color={color}
           />
+          </g>
         ))}
-
+        
         {main &&
           metrics.map((m,i) => (
+            <g key={`${i}-ps`}>
             <InfoPoints
-              key={`${i}-ps`}
-              data={seriesObj}
+              key={`${i}-ps-h`}
+              data={historicSeries}
+              prop={m}
+              x={x}
+              y={y}
+              color={hcolor}
+              mode="h"
+            />
+            <InfoPoints
+              key={`${i}-ps-f`}
+              data={futureSeries}
               prop={m}
               x={x}
               y={y}
               color={color}
+              mode="f"
             />
+            {activeInfo &&
+              <ShowActivePoint active={activeInfo} coord={[x(activeCoords.ts), y(activeCoords[m]), activeCoords[m]]} mode={activeMode} />
+            }
+            </g>
           ))}
+          
       </>
     )
   }
@@ -187,12 +236,11 @@ function ScenarioGraph({
       viewBox={`0 0 ${svgWidth} ${svgHeight}`}
     >
       <g transform={`translate(${lPad},${yPad})`}>
+        <rect x={x(ticksX[0])} width={x(ticksX[futureYear-15])} height={svgHeight-yPad-32-tPad} fill="#F5F5F5" />
         {ticksX.map((t, i) => {
           const xt = x(t)
           const yearLabel = 2000 + t
           const yearOutput = '‘' + yearLabel.toString().substr(-2)
-          //console.log(yearLabel)
-
           return (
             <g key={xt}>
               <line
@@ -201,35 +249,47 @@ function ScenarioGraph({
                 x2={xt}
                 y1={-5}
                 y2={height}
-                stroke="#D8D8D8"
                 {...(i === 0 || i === ticksX.length - 1
                   ? {}
-                  : { strokeDasharray: '4 3' })}
+                  : { strokeDasharray: '10 2' })}
+                {...(15+i === futureYear ? { strokeWidth: "2"} : {})}
+                {...(15+i === futureYear ? { stroke: "#E0E0E0"} : {stroke: "#D8D8D8"})}
               ></line>
-              <text x={xt} y={height + yPad} fontSize={12} textAnchor="middle">
+              <text x={xt} y={height + yPad-20} fontSize={12} textAnchor="middle">
                 {yearLabel}
               </text>
+              {(i < ticksX.length - 1) && <line
+                key={`${t}-year`}
+                x1={xt}
+                x2={x(t+1)-3}
+                y1={height + yPad}
+                y2={height + yPad}
+                stroke="#CCE8F4"
+                strokeWidth="10"
+              ></line>}
             </g>
           )
         })}
         {ticksY.map((t, i) => {
           const yt = y(t)
+          let yearBar = null
           return (
             <g key={yt}>
               <line
                 key={t}
                 x1={0}
-                x2={width - lPad}
+                x2={width - lPad - rPad}
                 y1={yt}
                 y2={yt}
                 stroke="#D8D8D8"
                 {...(i === 0 || i === ticksY.length - 1
                   ? {}
-                  : { strokeDasharray: '4 3' })}
+                  : { strokeDasharray: '10 2' })}
               ></line>
               <text x={-rPad} y={yt + 4} fontSize={12} textAnchor="middle">
                 {`${t}%`}
               </text>
+              {yearBar}
             </g>
           )
         })}
