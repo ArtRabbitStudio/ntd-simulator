7a5daecf1d8b816462f4dee54eb715d68387b960
Igor Clark <igor@artrabbit.com>
2020-10-12 19:07:02 +0100

    trachoma loads historical data & converts formats

--- src/pages/components/simulator/models/trachoma/index.js
+++ src/pages/components/simulator/models/trachoma/index.js
@@ -53,7 +53,99 @@ export default {
 
     console.log( 'TrachomaModel fetching prepped scenarioData', scenarioData );
 
-    const csvPromise = new Promise(
+    // convert '02-2020' to 20.16666666666666666
+    const convertDateIndex = ( key ) => {
+      const [ month, year ] = key.split('-' );
+      const ts = ( parseInt( year ) - 2000 ) + ( parseInt( month ) / 12 );
+      return ts;
+    };
+
+    const combineData = ( historicalData, futureData ) => {
+      return [ ...historicalData, ...futureData ]
+        .map(
+          // remove redundant columns
+          ( row ) => {
+            delete row[ 'Random Generator' ];
+            delete row.bet;
+            return row;
+          }
+        )
+        // for each row
+        .map(
+          ( row ) => {
+
+            return Object.keys( row ).reduce(
+
+              ( acc, key ) => {
+
+                // append it to the array of timestamps
+                const ts = convertDateIndex( key );
+                acc.ts.push( ts );
+
+                // append the prevalence value to the array of prevalences
+                const p = parseFloat( row[ key ] );
+                acc.p.push( p );
+
+                return acc;
+              },
+
+              // reduce()'s accumulator
+              { ts: [], p: [] }
+
+            )
+
+          }
+        );
+    };
+
+    /*
+     * comes in as
+     * {
+     *   "median": { "02-2020": 0.0526236376, },
+     *   "percentile_25": { "02-2020": 0.0269567667, },
+     *   "percentile_75": { "02-2020": 0.1019832762, }
+     * }
+     *
+     * goes out as
+     *
+     * {
+     *   "ts": [ 20.166666666666668, ],
+     *   "median": [ 0.0526236376, ],
+     *   "min": [ 0.0269567667, ],
+     *   "max": [ 0.1019832762, ]
+     * }
+     *
+     */
+
+    const convertSummary = ( s ) => {
+      return Object.keys( s.median ).reduce(
+
+        (acc, k ) => {
+
+          const ts = convertDateIndex( k );
+
+          acc.ts.push( ts );
+          acc.median.push( s.median[ k ] );
+          acc.min.push( s.percentile_25[ k ] );
+          acc.max.push( s.percentile_75[ k ] );
+
+          return acc;
+        },
+
+        { ts: [], median: [], min: [], max: [] }
+      );
+    };
+
+    const historicalDataPromise = new Promise(
+      ( resolve, reject ) => {
+        csv( "/data/Trachoma200/output/scenario-56/56-historical-prevalence.csv" )
+        .then( ( results ) => {
+          resolve( results );
+        } );
+      }
+    );
+
+    const futureDataPromise = new Promise(
       ( resolve, reject ) => {
         csv( "/data/Trachoma200/output/scenario-56/coverage-0.6/56-0.6-12-202001.csv" )
         .then( ( results ) => {
@@ -71,13 +163,15 @@ export default {
       }
     );
 
-    Promise.all( [ csvPromise, jsonPromise ] )
+    Promise.all( [ historicalDataPromise, futureDataPromise, jsonPromise ] )
       .then(
-        ( [ results, summary ] ) => {
+        ( [ historicalData, futureData, summaryData ] ) => {
+          const combinedData = combineData( historicalData, futureData );
+          const convertedSummary = convertSummary( summaryData );
           const result = {
             ...scenarioData,
-            results,
-            summary
+            results: combinedData,
+            summary: convertedSummary
           };
           callbacks.resultCallback( result, isNewScenario );
         }
