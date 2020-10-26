62d61526b18654e93e1728cab60ce2acdbe702bf
Igor Clark <igor@artrabbit.com>
2020-10-26 22:38:17 +0000

    update iu-level.csv for currently-generating IUs

--- src/pages/components/simulator/models/trachoma/index.js
+++ src/pages/components/simulator/models/trachoma/index.js
@@ -156,7 +156,7 @@ export default {
 
     // work out the data file path from the scenario settings
     const mappedGroup = window.ntd.iuGroupMapping[ simState.IUData.id ];  // TODO temporary - to be generated from full IU list and loaded into simState in iuLoader
-    const group = [ 59,60,61,62 ].includes( mappedGroup ) ? 103 : mappedGroup;
+    const group = [ 32,37,43,262,265,323,325,324,369,382,126,99,132,128,124,142 ].includes( mappedGroup ) ? mappedGroup : 32;
     const coverage = scenarioData.settings.coverage / 100;    // 0.9 in model vs 90 in UI
     const mdaSixMonths = scenarioData.settings.mdaSixMonths;  // 6=biannual, 12=annual
     const mdaRoundsString = scenarioData.mdaFuture.time
