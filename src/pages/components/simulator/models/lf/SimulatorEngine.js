da047b0fb97810694ba3545873aa4b9a1c233678
mattg3004 <matthewgraham3004@gmail.com>
2020-11-24 11:15:49 +0800

    add bednet coverage function

--- src/pages/components/simulator/models/lf/SimulatorEngine.js
+++ src/pages/components/simulator/models/lf/SimulatorEngine.js
@@ -246,11 +246,14 @@ export var Model = function (n) {
       //mf += param->kappas1 * pow(1 - exp(-param->r1 *( host_pop[i].mfConc() * host_pop[i].b)/param->kappas1), 2.0);
       mf += this.people[i].b * statFunctions.L3Uptake(this.people[i].M)
       bTot += this.people[i].b
+
     }
+    
     mf = mf / bTot //(double) n;
+    var bedNetCov = this.bedNetCoverage()
     return (
       (mf *
-        (1 + this.bedNetInt * params.covN * (params.sN - 1)) *
+        (1 + this.bedNetInt * bedNetCov * (params.sN - 1)) *
         params.lbda *
         params.g) /
       (params.sig + params.lbda * params.psi1)
@@ -283,7 +286,16 @@ export var Model = function (n) {
       }
     }
   }
-
+  
+  this.bedNetCoverage = function(){
+    var nts = 0.0
+    for (var i = 0; i < this.n; i++) {
+      //mf += param->kappas1 * pow(1 - exp(-param->r1 *( host_pop[i].mfConc() * host_pop[i].b)/param->kappas1), 2.0);
+      nts += this.people[i].bednet
+    }
+    return(nts/this.n)
+  }
+  
   this.bedNetEvent = function () {
     params.sig = params.sig + params.lbda * params.dN * params.covN
     var bedNetInc = (params.covN - params.covNOld) * this.n
